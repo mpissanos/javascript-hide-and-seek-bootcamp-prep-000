2e0a7030fe87c6ed8a6917e4de8dcbeeb58eeaed
Manuel Pissanos <mpissano@gmail.com>
2018-08-29 14:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app')querySelectorAll('ul.ranked-list li');
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for( let i = 0; i< lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i]) + n
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
   }
+}
 
+function deepestChild() {
+  
 }
