8fb8ee52f1b854173c5fc188fdb44eefcfad6491
Manuel Pissanos <mpissano@gmail.com>
2018-08-29 14:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget() {
-  return document.querySelector
+function nestedTarget(target) {
+  return document.querySelector()
 }
